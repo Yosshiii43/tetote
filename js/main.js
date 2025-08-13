292ed12f0b2bd2b111a712600d6d8e9058a2e160
Yosshiii43 <daisy440440@gmail.com>
2025-08-13 16:49:22 +0900

    add:header hero超えたらsticky

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
  * main.js  –  ver.1.0
  *************************************************************************/
 
+//--------------------------------------------------------------------------
+// ハンバーガーメニューとドロワー
+//--------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.body;
   const hamburger = document.querySelector('#js-hamburger');
@@ -59,6 +62,29 @@ document.addEventListener('DOMContentLoaded', () => {
     if (e.key === 'Escape') {
       e.preventDefault();
       closeMenu();
+    }
+  });
+});
+
+
+//--------------------------------------------------------------------------
+// スクロールするとアニメーション付きで追従ヘッダーを出すコード
+//--------------------------------------------------------------------------
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', function() {
+    const header = document.querySelector('.p-header');
+    const headerHeight = header.offsetHeight; // ヘッダーの高さを取得
+    const scrollY = window.pageYOffset;
+    const hero = document.querySelector('.p-main__head');
+    const heroHeight = hero.offsetHeight; // ヒーローの高さを取得
+    
+    
+    if (scrollY >= heroHeight) {
+      header.classList.add('p-header--sticky');
+      document.body.style.marginTop = headerHeight + 'px'; // コンテンツにヘッダーの高さ分の余白を設定
+    } else {
+      header.classList.remove('p-header--sticky');
+      document.body.style.marginTop = '0'; // コンテンツの余白をリセット
     }
   });
 });
