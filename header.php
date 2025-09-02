c485f66c49c1e98c1c85bbd87b33e37ca6d52532
Yosshiii43 <daisy440440@gmail.com>
2025-09-03 02:02:16 +0900

    change:c-lowerHero,breadcrumb共通化

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
             <li class="p-drawer__item"><a class="p-drawer__link" href="/about-us/"><span class="c-viga ">about us</span><span>TETOTEについて</span></a></li>
             <li class="p-drawer__item"><a class="p-drawer__link" href="/staff/"><span class="c-viga ">staff</span><span>社員について</span></a></li>
             <li class="p-drawer__item"><a class="p-drawer__link" href="/blog/"><span class="c-viga ">blog</span><span>採用ブログ</span></a></li>
-            <li class="p-drawer__item"><a class="p-drawer__link" href="/toptopBenefits/"><span class="c-viga ">topBenefits</span><span>福利厚生について</span></a></li>
+            <li class="p-drawer__item"><a class="p-drawer__link" href="/benefits/"><span class="c-viga ">benefits</span><span>福利厚生について</span></a></li>
             <li class="p-drawer__item"><a class="p-drawer__link" href="/career/"><span class="c-viga ">carrer</span><span>研修制度とキャリアパス</span></a></li>
             <li class="p-drawer__item"><a class="p-drawer__link" href="/faq/"><span class="c-viga ">FAQ</span><span>よくある質問</span></a></li>
           </ul>
@@ -62,3 +62,47 @@
       </div><!-- .p-drawer__menu -->
     </div><!-- .p-drawer__inner -->
   </div><!-- .p-drawer -->
+
+  <main class="l-main" id="main">
+    <div class="l-main__head p-main__head">
+
+      <?php
+      // --- LowerHero: 表示対象ページ（ホワイトリスト）と固定文言 ---
+      $lh = null;
+
+      if ( is_page('about-us') ) {
+        $lh = ['lead' => 'about us', 'title' => 'TETOTEについて', 'outline' => '私たちの理念を紹介をします。'];
+      } elseif ( is_post_type_archive('staff') ) {
+        $lh = ['lead' => 'staff', 'title' => '社員について', 'outline' => '弊社社員のリアルな声を紹介しています。'];
+      } elseif ( is_home() ) {
+        $lh = ['lead' => 'blog', 'title' => '採用ブログ', 'outline' => '採用情報やイベント情報などをご紹介します。'];
+      } elseif ( is_page('benefits') ) {
+        $lh = ['lead' => 'benefits', 'title' => '福利厚生について', 'outline' => '充実した福利厚生制度を設けています。'];
+      } elseif ( is_page('career') ) {
+        $lh = ['lead' => 'career', 'title' => '研修制度とキャリアパス', 'outline' => 'キャリアパスを支える充実した、研修制度'];
+      } elseif ( is_page('details') ) {
+        $lh = ['lead' => 'details', 'title' => '募集要項', 'outline' => 'TETOTEは幅広く仲間を募集しています。'];
+      } elseif ( is_page('faq') ) {
+        $lh = ['lead' => 'FAQ', 'title' => 'よくある質問', 'outline' => '皆様からよく頂くご質問にお答えします。'];
+      }
+      ?>
+
+      <?php if ( $lh ) : ?>
+        <div class="c-lowerHero">
+          <div class="l-inner--lower c-lowerHero__inner">
+            <p class="c-lowerHero__lead c-viga"><?php echo esc_html($lh['lead']); ?></p>
+            <h1 class="c-lowerHero__title"><?php echo esc_html($lh['title']); ?></h1>
+            <?php if (!empty($lh['outline'])): ?>
+              <p class="c-lowerHero__outline"><?php echo esc_html($lh['outline']); ?></p>
+            <?php endif; ?>
+          </div>
+        </div><!-- .p-lowerHero -->
+      <?php endif; ?>
+
+      <?php if ( ! is_front_page() && ! is_singular('staff') ) : ?>
+        <!-- ▼ breadcrumb =============================== -->
+        <?php breadcrumb(); ?>
+        <!-- ▲ breadcrumb =============================== -->
+      <?php endif; ?>
+
+    </div><!-- .l-main__head .p-main__head-->
