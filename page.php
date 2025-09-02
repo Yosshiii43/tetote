c9eeff7c42764d73cd1b96bf9116e58bea012aef
Yosshiii43 <daisy440440@gmail.com>
2025-09-03 02:03:23 +0900

    change:WordPress化

---
+++ page.php
@@ -0,0 +1,70 @@
+<?php get_header(); ?>
+
+<!-- ▼ content =========================================== -->
+<div class="content">
+
+  <!--ループ開始 -->
+  <?php if (have_posts()) : while (have_posts()) :
+    the_post(); ?>
+
+  <!-- ▼ tmp =========================================== -->
+
+  <!-- ▼その他（tmp/page/{slug}.php があればインクルード、親ページならば子ページのリストを表示） -->
+
+  <?php
+    // 現在のページのスラッグと親ページのスラッグを組み合わせてテンプレートパスを生成
+    $slug_name = $post->post_name;
+    $parent_slugs = ys_get_parent_slugs($post); 
+    $template_slug = 'tmp/page/' . (!empty($parent_slugs) ? implode('-', $parent_slugs) . '-' : '') . $slug_name . '.php';
+
+    // テンプレートファイルをインクルード
+      $template_path = ys_locate_page_template($post); 
+      if ($template_path) {
+        include $template_path; // 例: tmp/page/会社-アクセス.php や tmp/page/company-access.php など
+      } else {
+      // 子ページのタイトルを取得して目次として表示
+      $child_pages = get_pages(['child_of' => get_the_ID(), 'sort_column' => 'menu_order']);
+      if (!empty($child_pages)) : ?>
+        <div class="c-childPageToc">
+          <ul class="c-childPageToc__items">
+            <?php foreach ($child_pages as $child_page) : ?>
+              <li class="c-childPageToc__item">
+                <a href="<?php echo esc_url(get_permalink($child_page->ID)); ?>"
+                  class="c-childPageToc__link">
+                  <?php echo esc_html($child_page->post_title); ?>
+                </a>
+              </li>
+            <?php endforeach; ?>
+          </ul>
+        </div>
+      <?php endif;
+    }
+  ?>
+  <!-- ▲その他 -->
+
+  <!-- ▲ tmp =================================================== -->
+
+  <?php if(!empty($post->post_content)): //投稿画面から入力した本文を出力 ?>
+  <div class="wp-post">
+    <!-- 各コンポーネントブロックのクラス名はJSで付与 -->
+    <?php the_content();?>
+
+  </div>
+  <?php endif; ?>
+
+  <?php endwhile; else: //ページがないとき ?>
+  <?php if (is_404()): ?>
+  <?php get_template_part('tmp/page/page-404'); ?>
+  <?php else : ?>
+  <p>準備中です。</p>
+  <?php endif; ?>
+  <?php endif; ?>
+  <!--ループ終了 -->
+
+</div>
+<!-- ▲ content =========================================== -->
+
+</main>
+<!-- ▲ container =================================================== -->
+
+<?php get_footer(); ?>
---
+++ tmp/page/benefits.php
@@ -0,0 +1,78 @@
+
+
+    <div class="l-main__body">
+      <section class="p-benefitsVarious">
+        <div class="l-inner--lower p-benefitsVarious__inner">
+          <h2 class="p-benefitsVarious__title c-lowerSectionTitle">各種制度</h2>
+          <table class="p-benefitsVarious__table c-table">
+            <tbody>
+              <tr>
+                <th>手当</th>
+                <td>
+                    <p>・通勤手当（上限月5万円）</p>
+                    <p>・時間外割増手当</p>
+                    <p>・役職手当</p>
+                    <p>・退職金</p>
+                </td>
+              </tr>
+              <tr>
+                <th>制度</th>
+                <td>財形貯蓄</td>
+              </tr>
+              <tr>
+                <th>保険</th>
+                <td>社会保険完備</td>
+              </tr>
+              <tr>
+                <th>施設</th>
+                <td>
+                  <ul>
+                    <li>社員向けジム</li>
+                    <li>社内託児所</li>
+                  </ul>
+                </td>
+              </tr>
+              <tr>
+                <th>休暇・休日</th>
+                <td>
+                  <ul>
+                    <li>完全週休2日制</li>
+                    <li>年次有給休暇（初年度10日／年度途中入社は比例付与）</li>
+                    <li>年末年始休暇（12月29日～1月3日：6日間）</li>
+                    <li>夏季休暇（6月～9月中：5日間）</li>
+                    <li>慶弔休暇</li>
+                    <li>産前産後休暇</li>
+                    <li>リフレッシュ休暇（3年勤続で5日／以降5年勤続ごとに5日支給</li>
+                  </ul>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div><!-- p-benefitsVarious__inner -->
+      </section><!-- p-benefitsVarious -->
+
+      <section class="p-benefitsVoice">
+        <div class="l-inner--lower p-benefitsVoice__inner">
+          <h2 class="p-benefitsVoice__title c-lowerSectionTitle">制度利用者の声</h2>
+          <div class="p-benefitsVoice__contents">
+            <section class="p-benefitsVoice__unit">
+              <p>2013年入社</p>
+              <h3>堀内 優</h3>
+              <p>資格取得のための費用を会社が負担してくれる制度を利用して、念願の応用技術者資格を取得できました。<br>
+                  この制度は、自己啓発を支援してくれるので、とても助かります。今後は、さらにスキルアップを目指して、他の資格も取得していきたいと思っています。<br>
+                  会社が成長を支援してくれるので、安心して仕事に取り組むことができます。</p>
+            </section>
+
+            <section class="p-benefitsVoice__unit">
+              <p>2015年入社</p>
+              <h3>神凪 理沙</h3>
+              <p>出産後、育休を取得して、現在は短時間勤務で働いています。会社が育児に理解があるので、安心して仕事に集中できます。<br>
+                  特に、社内託児所があるので、子供を預けながら安心して働けるのは助かります。また、短時間勤務制度も充実しているので、自分のペースで仕事復帰することができました。<br>
+                  今後は、子供の成長に合わせて、勤務時間を調整しながら、長く働き続けたいと思っています。</p>
+            </section>
+          </div><!-- p-benefitsVoice__contents -->
+          <img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_benefitsUserVoices1.jpg" alt="微笑むスーツ姿の男性社員と女性社員">
+        </div><!-- l-inner--lower p-benefitsVoice__inner -->
+      </section><!-- p-benefitsVoice -->
+    </div><!-- l-main__body -->
+  </main>
