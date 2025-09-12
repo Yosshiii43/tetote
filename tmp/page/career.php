a9c223e10b155dad8298b61ce857ad51c937c2ef
Yosshiii43 <daisy440440@gmail.com>
2025-09-12 19:15:16 +0900

    change:WordPress化

---
+++ tmp/page/career.php
@@ -0,0 +1,49 @@
+
+
+    <div class="l-main__body">
+      <section class="p-careerPass">
+        <div class="l-inner--lower p-careerPass__inner">
+          <h2 class="p-careerPass__title c-lowerSectionTitle">キャリアパスに沿った研修制度を用意</h2>
+          <div class="p-careerPass__contents">
+            <p class="p-careerPass__description">弊社では、コンサルタントのキャリアパスを、大きく4つのステージに分けています。</p>
+            <div class="p-careerPass__img" >
+              <div class="inner">
+                <img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_tetoteCarrerPath1.png" 
+                    alt="TETOTEでのキャリアパス *コンサルタントの場合
+                          01 コンサルタント 入社後2〜3年 プロジェクトチームの一員として、データ分析、資料作成、クライアントとの調整などを担当
+                          02 シニアコンサルタント コンサルタント経験3〜5年 プロジェクトリーダーとして、プロジェクト全体の指揮を執る
+                          03 マネージャー シニアコンサルタント経験5年以上 複数のプロジェクトを管理し、コンサルタントの指導・育成
+                          04 パートナー マネージャー経験10年以上 コンサルティングファームの経営に参画 新規事業の立ち上げ、クライアントとの関係構築">
+              </div>
+            </div>
+            <p class="p-careerPass__introduction">TETOTE は、コンサルタントのキャリアパスを支えるために、以下の独自の研修制度を設けています。</p>
+            <section class="p-careerPass__unit">
+              <h3>1.ケーススタディ研修</h3>
+              <p class="p-careerPass__text">TETOTE の過去のプロジェクト事例を基にしたケーススタディ研修を実施しています。<br>
+                  この研修では、実際のプロジェクトで直面する課題を解決するために、分析力、問題解決能力、プレゼンテーション能力などを実践的に習得することができます。</p>
+            </section>
+            <section class="p-careerPass__unit">
+              <h3>2. クライアント対応研修</h3>
+              <p class="p-careerPass__text">TETOTE は、クライアントとの信頼関係構築を重視しています。<br>
+                  この研修では、クライアントとのコミュニケーションスキル、交渉スキル、プレゼンテーションスキルなどを習得し、クライアントの課題解決に貢献できるコンサルタントを育成します。</p>
+            </section>
+            <section class="p-careerPass__unit">
+              <h3>3. 専門知識研修</h3>
+              <p class="p-careerPass__text">財務、会計、ITなど、コンサルタント業務に必要な専門知識を習得するための研修を実施しています。<br>
+                  TETOTE は、幅広い分野の知識を持つコンサルタントを育成し、クライアントに最適なソリューションを提供できるようにしています。</p>
+            </section>
+            <section class="p-careerPass__unit">
+              <h3>4. マネジメント研修</h3>
+              <p class="p-careerPass__text">プロジェクトマネジメント、リーダーシップ、チームビルディングなど、マネジメントスキルを習得するための研修を実施しています。<br>
+                  TETOTE は、将来的にマネジメント層を担うコンサルタントを育成し、組織全体の成長を促進しています。</p>
+            </section>
+            <section class="p-careerPass__unit">
+              <h3>5. 外部研修</h3>
+              <p class="p-careerPass__text">社外で実施される研修にも積極的に参加することができます。<br>
+                  TETOTE は、最新の知識やスキルを習得し、常に成長し続けるコンサルタントを育成します。</p>
+            </section>
+          </div><!-- p-careerPass__contents -->
+        </div><!-- l-inner--lower p-careerPass__inner -->
+      </section><!-- p-careerPass--->
+    </div><!-- l-main__body -->
+  </main>
