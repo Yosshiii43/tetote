0a6a77f331e4cbf34640f66512189a0825c7e764
Yosshiii43 <daisy440440@gmail.com>
2025-09-12 19:23:04 +0900

    change:WordPress化

---
+++ tmp/page/details.php
@@ -0,0 +1,159 @@
+    <div class="l-main__body">
+      <div class="p-details">
+        <div class="l-inner--lower p-details__inner">
+          <p class="p-details__catchcopy">あなたが輝ける場所が、ここにはある</p>
+          <p class="p-details__description">TETOTEでは、コンサルタントを中心に、様々な職種を募集しています。</p>
+          <ul class="c-lowerMenu p-details__menu">
+            <li class="c-lowerMenu__button"><a href="#consultant"><span>コンサルタント</span></a></li>
+            <li class="c-lowerMenu__button"><a href="#sales"><span>ソリューション営業</span></a></li>
+            <li class="c-lowerMenu__button"><a href="#engineer"><span>システムエンジニア</span></a></li>
+          </ul>
+          <div class="p-details__contents">
+            <section class="p-details__unit" id="consultant">
+              <h2 class="p-details__title c-lowerSectionTitle">募集要項-コンサルタント</h2>
+              <table class="c-table">
+                <tbody>
+                  <tr>
+                    <th>職種名</th>
+                    <td>コンサルタント</td>
+                  </tr>
+                  <tr>
+                    <th>雇用形態</th>
+                    <td>正社員</td>
+                  </tr>
+                  <tr>
+                    <th>業務内容</th>
+                    <td>クライアントの課題解決のためのコンサルティング業務</td>
+                  </tr>
+                  <tr>
+                    <th>就業時間</th>
+                    <td>9:00～18:00（フレックスタイム制）</td>
+                  </tr>
+                  <tr>
+                    <th>給与</th>
+                    <td>年収500万円～（経験・能力による）</td>
+                  </tr>
+                  <tr>
+                    <th>勤務地</th>
+                    <td>東京、大阪、名古屋</td>
+                  </tr>
+                  <tr>
+                    <th>転勤の有無</th>
+                    <td>あり</td>
+                  </tr>
+                  <tr>
+                    <th>休日休暇</th>
+                    <td>土日祝日、年末年始、夏季休暇</td>
+                  </tr>
+                  <tr>
+                    <th>選考について</th>
+                    <td>書類選考、面接</td>
+                  </tr>
+                  <tr>
+                    <th>採用人数</th>
+                    <td>若干名</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section><!-- p-details__unit -->
+
+            <section class="p-details__unit" id="sales">
+              <h2 class="p-details__title c-lowerSectionTitle">募集要項-ソリューション営業</h2>
+              <table class="c-table">
+                <tbody>
+                  <tr>
+                    <th>職種名</th>
+                    <td>ソリューション営業</td>
+                  </tr>
+                  <tr>
+                    <th>雇用形態</th>
+                    <td>正社員</td>
+                  </tr>
+                  <tr>
+                    <th>業務内容</th>
+                    <td>顧客の課題解決のためのソリューション提案、販売</td>
+                  </tr>
+                  <tr>
+                    <th>就業時間</th>
+                    <td>9:00～18:00（フレックスタイム制）</td>
+                  </tr>
+                  <tr>
+                    <th>給与</th>
+                    <td>年収500万円～（経験・能力による）</td>
+                  </tr>
+                  <tr>
+                    <th>勤務地</th>
+                    <td>東京、大阪、名古屋</td>
+                  </tr>
+                  <tr>
+                    <th>転勤の有無</th>
+                    <td>あり</td>
+                  </tr>
+                  <tr>
+                    <th>休日休暇</th>
+                    <td>土日祝日、年末年始、夏季休暇</td>
+                  </tr>
+                  <tr>
+                    <th>選考について</th>
+                    <td>書類選考、面接</td>
+                  </tr>
+                  <tr>
+                    <th>採用人数</th>
+                    <td>若干名</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section><!-- p-details__unit -->
+
+            <section class="p-details__unit" id="engineer">
+              <h2 class="p-details__title c-lowerSectionTitle">募集要項-システムエンジニア</h2>
+              <table class="c-table">
+                <tbody>
+                  <tr>
+                    <th>職種名</th>
+                    <td>システムエンジニア</td>
+                  </tr>
+                  <tr>
+                    <th>雇用形態</th>
+                    <td>正社員</td>
+                  </tr>
+                  <tr>
+                    <th>業務内容</th>
+                    <td>システム開発、インフラ構築、データ分析など</td>
+                  </tr>
+                  <tr>
+                    <th>就業時間</th>
+                    <td>9:00～18:00（フレックスタイム制）</td>
+                  </tr>
+                  <tr>
+                    <th>給与</th>
+                    <td>年収500万円～（経験・能力による）</td>
+                  </tr>
+                  <tr>
+                    <th>勤務地</th>
+                    <td>東京、大阪、名古屋</td>
+                  </tr>
+                  <tr>
+                    <th>転勤の有無</th>
+                    <td>あり</td>
+                  </tr>
+                  <tr>
+                    <th>休日休暇</th>
+                    <td>土日祝日、年末年始、夏季休暇</td>
+                  </tr>
+                  <tr>
+                    <th>選考について</th>
+                    <td>書類選考、面接</td>
+                  </tr>
+                  <tr>
+                    <th>採用人数</th>
+                    <td>若干名</td>
+                  </tr>
+                </tbody>
+              </table>
+            </section><!-- p-details__unit -->
+          </div>
+        </div>
+      </div> 
+    </div><!-- l-main__body -->
+  </main>
