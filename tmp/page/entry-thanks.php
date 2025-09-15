f81f641e455b9cccf52c472eaf980f00890c62f5
Yosshiii43 <daisy440440@gmail.com>
2025-09-15 19:05:43 +0900

    add:entry-thanksページ

---
+++ tmp/page/entry-thanks.php
@@ -0,0 +1,11 @@
+    <div class="l-main__body">
+      <div class="p-entryThanks">
+        <div class="l-inner--entry p-entryThanks__inner">
+          <div class="p-entryThanks__contents">
+            <h1>送信ありがとうございました。</h1>
+            <p>3営業日以内に人事担当者より連絡させていただきます。</p>
+          </div><!-- p-entryThanks__contents -->
+        </div><!-- l-inner--entryThanks p-entryThanks__inner -->
+      </div><!-- p-entryThanks -->
+    </div><!-- l-main__body -->
+  </main>
