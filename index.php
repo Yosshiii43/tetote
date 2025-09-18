6ca5049cf8ceb182326a416188cf8ba8cbf067a5
Yosshiii43 <daisy440440@gmail.com>
2025-09-18 20:11:25 +0900

    fix:theme復元

---
+++ index.php
@@ -0,0 +1,284 @@
+<?php get_header(); ?>
+
+    <div class="l-main__head p-main__head">
+      <div class="p-hero">
+        <!-- Slider main container -->
+        <div class="swiper">
+          <!-- Additional required wrapper -->
+          <div class="swiper-wrapper">
+            <!-- Slides -->
+            <div class="swiper-slide">
+              <div class="slide-media" style="--hero-x:64%;">
+                <img  class="p-hero__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_hero_top1.jpg" alt="街路樹が美しい街をスーツ姿で歩く20代男性" loading="eager" decoding="async">
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="slide-media" style="--hero-x:50%;">
+                <img class="p-hero__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_hero_top2.jpg" alt="オフィス内で笑顔で打ち合わせをする一人の男性と二人の女性">
+              </div>
+            </div>
+          </div>
+        </div><!-- .swiper -->
+
+        <p class="p-hero__catchcopy p-heroCatchcopy c-viga">BECOME A <br>CHALLENGER.</p>
+
+        <div class="p-hero__text p-heroText"><p>君の挑戦が、意思が、未来を変える</p></div>
+
+        <div class="p-hero__news p-heroNews">
+          <div class="p-heroNews__head">
+            <h2 class="c-viga">news</h2>
+          </div>
+          <div class="p-heroNews__body">
+            <a href="@@後で入れる@@">
+              <span class="js-lineHeight">2024年度働きやすさランキング一位を取得しました！！</span>
+              <span><span class="c-viga">view more</span><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/icon_arrow1.svg" alt="" width="20" height="20"></span>
+            </a>
+          </div>
+        </div>
+
+      </div><!-- .p-hero -->
+
+    </div><!-- .l-main__head .p-main__head-->
+
+    <div class="l-main__body">
+
+      <div class="p-mission" id="topMission">
+        <div class="p-mission__head">
+          <div class="swiper">
+            <div class="swiper-wrapper">
+              <div class="swiper-slide"><div class="slide-media"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide1.jpg" alt="オフィスで電話をかけながらキーボードを操作するスーツ姿の30代男性" width="1012" height="786"></div></div>
+              <div class="swiper-slide"><div class="slide-media slide-media-down"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide2.jpg" alt="カフェでノートパソコンを操作している20代女性" width="1012" height="786"></div></div>
+              <div class="swiper-slide"><div class="slide-media"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide3.jpg" alt="お客様と向かい合って説明をしているスーツ姿の20代女性" width="1012" height="786"></div></div>
+              <div class="swiper-slide"><div class="slide-media slide-media-down"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide1.jpg" alt="" width="1012" height="786"></div></div>
+              <div class="swiper-slide"><div class="slide-media"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide2.jpg" alt="" width="1012" height="786"></div></div>
+              <div class="swiper-slide"><div class="slide-media slide-media-down"><img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_slide3.jpg" alt="" width="1012" height="786"></div></div>
+            </div>
+          </div>
+          <div class="p-mission__text">
+            <p>後悔しないキャリアを作る、</p>
+            <p>それこそが、我々の使命だ</p>
+          </div>
+        </div><!-- .p-mission__head-->
+        <div class="p-mission__body">
+          <div>
+            <p>「人手不足」<br>
+              <span><span>今の日本が抱える</span><span>この社会課題に挑み、</span></span>
+              <span><span>企業と個人の可能性を</span><span>最大限に引き出す。</span></span>
+              それが私達の役目。
+            </p>
+            <p>単につなぐだけじゃない。
+              <span><span>「手と手」を取り合って</span><span>いけるような、</span></span>
+              <span><span>持続可能な社会を、</span><span>一緒に作りませんか？</span></span>
+            </p>
+          </div>
+          <a class="c-button--wb c-viga" href="/about-us/">view more</a>
+        </div>
+      </div><!-- .p-mission -->
+
+      <section class="p-member" id="topMember">
+        <div class="l-inner--1024">
+          <h2 class=" p-member__title">
+            <span class="c-topSectionTitle__upper"><span>人</span>を知る</span>
+            <span class="c-topSectionTitle__lower c-poppins">member</span>
+          </h2>
+          <div class="p-member__description">
+            <p>TETOTEの社員がどういった信念を持って働いているのか、<br>
+                一日のスケジュールや仕事内容などを紹介します。</p>
+          </div>
+          <div class="p-member__swiper">
+            <div class="p-member__swiperInner">
+              <div class="swiper">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <article class="c-memberCard">
+                      <div class="c-memberCard__head">
+                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member01.jpg" alt="西村優ポートレート" width="600" height="758">
+                        <div class="c-memberCard__text">
+                          <p>「あなたが担当で良かった」</p>
+                          <p>この一言が、最高のやりがい</p>
+                        </div>
+                      </div>
+                      <div class="c-memberCard__body">
+                        <div class="c-memberCard__info">
+                          <p>コンサルタント</p>
+                          <p>2011年<span>入社</span></p>
+                        </div>
+                        <h3><span>西村</span><span>優</span></h3>
+                      </div>
+                    </article><!-- c-memberCard" -->
+                  </div><!-- .swiper-slide --> 
+
+                  <div class="swiper-slide">
+                    <article class="c-memberCard">
+                      <div class="c-memberCard__head">
+                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member02.jpg" alt="橋本拓也ポートレート" width="600" height="758">
+                        <div class="c-memberCard__text">
+                          <p>「あなたが担当で良かった」</p>
+                          <p>この一言が、最高のやりがい</p>
+                        </div>
+                      </div>
+                      <div class="c-memberCard__body">
+                        <div class="c-memberCard__info">
+                          <p>コンサルタント</p>
+                          <p>2011年<span>入社</span></p>
+                        </div>
+                        <h3><span>西村</span><span>優</span></h3>
+                      </div>
+                    </article><!-- c-memberCard" -->
+                  </div><!-- .swiper-slide --> 
+
+                  <div class="swiper-slide">
+                    <article class="c-memberCard">
+                      <div class="c-memberCard__head">
+                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member03.jpg" alt="青木美月ポートレート" width="600" height="758">
+                        <div class="c-memberCard__text">
+                          <p>「あなたが担当で良かった」</p>
+                          <p>この一言が、最高のやりがい</p>
+                        </div>
+                      </div>
+                      <div class="c-memberCard__body">
+                        <div class="c-memberCard__info">
+                          <p>コンサルタント</p>
+                          <p>2011年<span>入社</span></p>
+                        </div>
+                        <h3><span>西村</span><span>優</span></h3>
+                      </div>
+                    </article><!-- c-memberCard" -->
+                  </div><!-- .swiper-slide --> 
+
+                  <div class="swiper-slide">
+                    <article class="c-memberCard">
+                      <div class="c-memberCard__head">
+                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member04.jpg" alt="佐々木健ポートレート" width="600" height="758">
+                        <div class="c-memberCard__text">
+                          <p>「あなたが担当で良かった」</p>
+                          <p>この一言が、最高のやりがい</p>
+                        </div>
+                      </div>
+                      <div class="c-memberCard__body">
+                        <div class="c-memberCard__info">
+                          <p>コンサルタント</p>
+                          <p>2011年<span>入社</span></p>
+                        </div>
+                        <h3><span>西村</span><span>優</span></h3>
+                      </div>
+                    </article><!-- c-memberCard" -->
+                  </div><!-- .swiper-slide --> 
+                </div><!-- .swiper-wrapper -->
+              </div><!-- .swiper -->
+            </div>
+          </div><!-- .p-member__swiper -->
+          <a class="c-button--wb c-viga" href="/staff/">view more</a>
+          <div class="swiper-controller">
+            <button class="swiper-button-prev c-button--arrowW"></button>
+            <button class="swiper-button-next c-button--arrowW"></button>
+          </div>
+        </div><!-- .l-inner--1024 -->
+      </section><!-- .p-member-->
+
+      <section class="p-topBenefits" id="toptopBenefits">
+        <div class="l-inner--1024">
+          <h2 class="c-topSectionTitle p-topBenefits__title">
+            <span class="c-topSectionTitle__upper"><span>制度・環境</span>を知る</span>
+            <span class="c-topSectionTitle__lower c-poppins">topBenefits</span>
+          </h2>
+          <div class="p-topBenefits__description">
+            <p>当社では働く従業員とそのご家族が健やかに過ごせるよう、多様な研修、福利厚生を提供しています。</p>
+          </div>
+          <div class="p-topBenefits__contents">
+            <div class="p-topBenefits__content">
+              <a href="/career/">
+                <img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_career1.png" alt="Training And Career">
+                <div class="c-button--arrowB"></div>
+                <h3>研修制度とキャリアパス</h3>
+                <p>個々の目標に合わせたキャリアパスを支える、豊富な研修メニューで、あなた自身の成長を強力にサポートします。</p>
+              </a>
+            </div>
+            <div class="p-topBenefits__content">
+              <a href="/topBenefits/">
+                <img src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_benefits1.png" alt="employee topBenefits">
+                <div class="c-button--arrowB"></div>
+                <h3>福利厚生</h3>
+                <p>TETOTEの福利厚生制度は、従業員の健康と幸福を重視し、働きやすい環境を提供することを目的としています。</p>
+              </a>
+            </div>
+          </div><!-- .p-topBenefits__contents -->
+        </div>
+      </section><!-- .p-topBenefits -->
+
+      <section class="p-blog" id="topBlog">
+        <div class="l-inner--1024">
+          <h2 class="c-topSectionTitle p-blog__title">
+            <span class="c-topSectionTitle__upper">採用ブログ</span>
+            <span class="c-topSectionTitle__lower c-poppins">blog</span>
+          </h2>
+          <div class="p-blog__description">
+            <p>採用情報やイベント情報、社員の紹介など、<br>
+              日々の現場の様子をご紹介します。</p>
+          </div>
+          <div class="p-blog__contents">
+            <div class="p-blog__content c-blogUnit">
+              <a href="/@@後で入れる@@">
+                <img class="c-blogUnit__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_blog1.jpg" alt="研修会場で話す女性講師">
+                <div class="c-blogUnit__text">
+                  <p class="c-blogUnit__category">社内研修</p>
+                  <p class="c-blogUnit__link">新入社員向けに、入社前研修を行いました。</p>
+                  <p class="c-blogUnit__date">2025.03.25</p>
+                </div>
+              </a>
+            </div>
+            <div class="p-blog__content c-blogUnit">
+              <a href="/@@後で入れる@@">
+                <img class="c-blogUnit__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_blog2.jpg" alt="研修会場">
+                <div class="c-blogUnit__text">
+                  <p class="c-blogUnit__category">社内研修</p>
+                  <p class="c-blogUnit__link">内定者向け研修を行いました。</p>
+                  <p class="c-blogUnit__date">2024.08.25</p>
+                </div>
+              </a>
+            </div>
+            <div class="p-blog__content c-blogUnit">
+              <a href="/@@後で入れる@@">
+                <img class="c-blogUnit__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_blog3.jpg" alt="景色を眺めるリュックサックを背負った人たちの後ろ姿">
+                <div class="c-blogUnit__text">
+                  <p class="c-blogUnit__category">社内イベント</p>
+                  <p class="c-blogUnit__link">【社員旅行2023】沖縄でリフレッシュ！チームワークも深まった！</p>
+                  <p class="c-blogUnit__date">2024.03.25</p>
+                </div>
+              </a>
+            </div>
+            <div class="p-blog__content c-blogUnit">
+              <a href="/@@後で入れる@@">
+                <img class="c-blogUnit__img" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_blog4.jpg" alt="腕組みをして笑顔を見せる男性社員と女性社員">
+                <div class="c-blogUnit__text">
+                  <p class="c-blogUnit__category">お知らせ</p>
+                  <p class="c-blogUnit__link">【新卒採用2024】エントリー受付中！</p>
+                  <p class="c-blogUnit__date">2024.03.01</p>
+                </div>
+              </a>
+            </div>
+          </div><!-- .p-blog__contents -->
+          <a class="p-blog__viewMore c-viga" href="/blog/"><span class="c-button--arrowW"></span>view more</a>
+        </div><!-- .l-inner--1024 -->
+      </section><!-- .p-blog -->
+
+      <section class="p-recruitment" id="topRecruitment">
+         <div class="l-inner--1024">
+          <h2 class="c-topSectionTitle p-recruitment__title">
+            <span class="c-topSectionTitle__upper">採用情報</span>
+            <span class="c-topSectionTitle__lower c-poppins">recruitment</span>
+          </h2>
+          <div class="p-recruitment__description">
+            <p>募集要項（職種、業務内容、応募条件、選考フロー）とよくある質問・会社概要などをまとめています。</p>
+          </div>
+          <div class="p-recruitment__contents">
+            <a class="p-recruitment__link c-button--wb" href="/details/">募集要項</a>
+            <a class="p-recruitment__link c-button--wb" href="/faq/">よくある質問</a>
+            <a class="p-recruitment__link c-button--wb" href="/about-us/#profile">会社概要</a>
+          </div>
+        </div>
+      </section>
+    </div><!-- l-main__body -->
+  </main>
+
+<?php get_footer(); ?>
