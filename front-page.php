6b96bd9f82efc6a6e12ded2214b92293d75d2f6a
Yosshiii43 <daisy440440@gmail.com>
2025-09-21 16:47:36 +0900

    add:member-swiper取得

--- front-page.php
+++ front-page.php
@@ -87,8 +87,8 @@
       </div><!-- .p-mission -->
 
       <section class="p-member" id="topMember">
-        <div class="l-inner--member">
-          <h2 class=" p-member__title">
+        <div class="l-inner--1024">
+          <h2 class="c-topSectionTitle p-member__title">
             <span class="c-topSectionTitle__upper"><span>人</span>を知る</span>
             <span class="c-topSectionTitle__lower c-poppins">member</span>
           </h2>
@@ -100,81 +100,89 @@
             <div class="p-member__swiperInner">
               <div class="swiper">
                 <div class="swiper-wrapper">
-                  <div class="swiper-slide">
-                    <article class="c-memberCard">
-                      <div class="c-memberCard__head">
-                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member01.jpg" alt="西村優ポートレート" width="600" height="758">
-                        <div class="c-memberCard__text">
-                          <p>「あなたが担当で良かった」</p>
-                          <p>この一言が、最高のやりがい</p>
-                        </div>
-                      </div>
-                      <div class="c-memberCard__body">
-                        <div class="c-memberCard__info">
-                          <p>コンサルタント</p>
-                          <p>2011年<span>入社</span></p>
-                        </div>
-                        <h3><span>西村</span><span>優</span></h3>
-                      </div>
-                    </article><!-- c-memberCard" -->
-                  </div><!-- .swiper-slide --> 
+                  <?php $member_query = new WP_Query(
+                    array(
+                      'post_type' => 'staff',
+                      'posts_per_page' => 8, //記事数
+                    )
+                  ); ?>
+                  <?php if ($member_query->have_posts()) : ?>
+                    <?php while ($member_query->have_posts()) : ?>
+                      <?php $member_query->the_post(); ?>
+                  
+                  <?php
+                    // ACFフィールド取得
+                    $staff_photo   = get_field('staff_photo');
+                    $staff_messages = get_field('staff_messages');
+                    $message_1     = $staff_messages['message_1'] ?? '';
+                    $message_2     = $staff_messages['message_2'] ?? '';
+                    $staff_name    = get_field('staff_name');
+                    $last_name     = $staff_name['last_name'] ?? '';
+                    $first_name    = $staff_name['first_name'] ?? '';
+                    $join_year     = get_field('join_year');
+                    $job_type      = wp_get_post_terms(get_the_ID(), 'job_type', ['fields' => 'names']);
+                    $job_type_name = $job_type ? $job_type[0] : '';
+                    $staff_photo_field = get_field('staff_photo');
+                    $photo_id = null;
+                    $photo_url = '';
+                    $alt = '';
 
+                    // ACF の返り値が配列の場合
+                    if ( is_array($staff_photo_field) && ! empty($staff_photo_field['id']) ) {
+                        $photo_id = intval( $staff_photo_field['id'] );
+                        $alt = $staff_photo_field['alt'];
+                    } elseif ( is_numeric($staff_photo_field) ) {
+                        // ACF が ID を返す設定の場合
+                        $photo_id = intval($staff_photo_field);
+                    } elseif ( is_array($staff_photo_field) && ! empty($staff_photo_field['url']) ) {
+                        // URL を返す設定の場合
+                        $photo_url = $staff_photo_field['url'];
+                    }
+                  ?>
                   <div class="swiper-slide">
                     <article class="c-memberCard">
-                      <div class="c-memberCard__head">
-                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member02.jpg" alt="橋本拓也ポートレート" width="600" height="758">
-                        <div class="c-memberCard__text">
-                          <p>「あなたが担当で良かった」</p>
-                          <p>この一言が、最高のやりがい</p>
+                      <a href="<?php the_permalink(); ?>">
+                        <div class="c-memberCard__head">
+                          <?php if ( $photo_id ) {
+                              // wp_get_attachment_image_src でURLとサイズを取得
+                              $img_data = wp_get_attachment_image_src( $photo_id, 'medium' );
+                              if ( $img_data ) {
+                                  echo '<img class="c-memberCard__media" src="' . esc_url($img_data[0]) . '" alt="' . esc_attr($alt) . '" width="' . esc_attr($img_data[1]) . '" height="' . esc_attr($img_data[2]) . '">';
+                              }
+                              } elseif ( ! empty($photo_url) ) {
+                                  // URLで返ってきた場合はサイズが不明 → width/heightなし
+                                  echo '<img class="c-memberCard__media" src="' . esc_url($photo_url) . '" alt="' . esc_attr($alt) . '">';
+                              } elseif ( has_post_thumbnail() ) {
+                                  // サムネイルあり
+                                  $thumb_id = get_post_thumbnail_id();
+                                  $img_data = wp_get_attachment_image_src( $thumb_id, 'medium' );
+                                  echo '<img class="c-memberCard__media" src="' . esc_url($img_data[0]) . '" alt="' . esc_attr($alt) . '" width="' . esc_attr($img_data[1]) . '" height="' . esc_attr($img_data[2]) . '">';
+                              } else {
+                                  // プレースホルダー
+                                  echo '<img class="c-memberCard__media" src="http://placehold.jp/600x758.png?text=No Image" alt="' . esc_attr($alt) . '" width="600" height="758">';
+                              }
+                              ?>
+                          <div class="c-memberCard__text">
+                            <?php if ($message_1): ?><p><?php echo esc_html($message_1); ?></p><?php endif; ?>
+                            <?php if ($message_2): ?><p><?php echo esc_html($message_2); ?></p><?php endif; ?>
+                          </div>
                         </div>
-                      </div>
-                      <div class="c-memberCard__body">
-                        <div class="c-memberCard__info">
-                          <p>コンサルタント</p>
-                          <p>2011年<span>入社</span></p>
+                        <div class="c-memberCard__body">
+                          <div class="c-memberCard__info">
+                            <p><?php echo esc_html($job_type_name); ?></p>
+                            <p><?php echo esc_html($join_year); ?><span>年入社</span></p>
+                          </div>
+                          <h2><span><?php echo esc_html($last_name); ?></span><span><?php echo esc_html($first_name); ?></span></h2>
                         </div>
-                        <h3><span>西村</span><span>優</span></h3>
-                      </div>
-                    </article><!-- c-memberCard" -->
+                      </a>
+                    </article>
                   </div><!-- .swiper-slide --> 
+                  <?php endwhile; ?>
+                  <?php else: ?>
+                    <p>記事がありません</p>
+                  <?php endif; ?>
+                  <?php wp_reset_postdata(); ?>
 
-                  <div class="swiper-slide">
-                    <article class="c-memberCard">
-                      <div class="c-memberCard__head">
-                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member03.jpg" alt="青木美月ポートレート" width="600" height="758">
-                        <div class="c-memberCard__text">
-                          <p>「あなたが担当で良かった」</p>
-                          <p>この一言が、最高のやりがい</p>
-                        </div>
-                      </div>
-                      <div class="c-memberCard__body">
-                        <div class="c-memberCard__info">
-                          <p>コンサルタント</p>
-                          <p>2011年<span>入社</span></p>
-                        </div>
-                        <h3><span>西村</span><span>優</span></h3>
-                      </div>
-                    </article><!-- c-memberCard" -->
-                  </div><!-- .swiper-slide --> 
-
-                  <div class="swiper-slide">
-                    <article class="c-memberCard">
-                      <div class="c-memberCard__head">
-                        <img class="c-memberCard__media" src="<?php echo esc_url(get_theme_file_uri()); ?>/img/img_member04.jpg" alt="佐々木健ポートレート" width="600" height="758">
-                        <div class="c-memberCard__text">
-                          <p>「あなたが担当で良かった」</p>
-                          <p>この一言が、最高のやりがい</p>
-                        </div>
-                      </div>
-                      <div class="c-memberCard__body">
-                        <div class="c-memberCard__info">
-                          <p>コンサルタント</p>
-                          <p>2011年<span>入社</span></p>
-                        </div>
-                        <h3><span>西村</span><span>優</span></h3>
-                      </div>
-                    </article><!-- c-memberCard" -->
-                  </div><!-- .swiper-slide --> 
                 </div><!-- .swiper-wrapper -->
               </div><!-- .swiper -->
             </div>
@@ -236,7 +244,7 @@
             ); ?>
           <?php if ($blog_query->have_posts()) : ?>
             <?php while ($blog_query->have_posts()) : ?>
-            <?php $blog_query->the_post(); ?>
+              <?php $blog_query->the_post(); ?>
               <?php get_template_part('tmp/module/blog-card'); ?>
             <?php endwhile; ?>
             <?php else: ?>
