d33562736966af6d1db3e095659dece348aec70e
Yosshiii43 <daisy440440@gmail.com>
2025-09-24 02:50:31 +0900

    add:sample post

--- single-post.php
+++ single-post.php
@@ -11,84 +11,83 @@ get_header(); ?>
     <!-- ▲ breadcrumb =============================== -->
      
     <div class="l-main__body">
-      <div class="l-inner--800 p-singleBlog__inner">
-        <div class="p-singleBlog__contents">
+      <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
 
-          <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
+        // カテゴリ(最初のカテゴリ)
+        $cats = get_the_category();
+        $cat_name = '';
+        if ( ! empty( $cats ) && ! is_wp_error( $cats ) ) {
+          $cat_name = esc_html( $cats[0]->name );
+        }
 
-            // カテゴリ(最初のカテゴリ)
-            $cats = get_the_category();
-            $cat_name = '';
-            if ( ! empty( $cats ) && ! is_wp_error( $cats ) ) {
-              $cat_name = esc_html( $cats[0]->name );
-            }
+        // 日付フォーマット
+        $date = get_the_modified_time( 'Y.m.d' );
 
-            // 日付フォーマット
-            $date = get_the_modified_time( 'Y.m.d' );
-
-            // サムネイル
-            if ( has_post_thumbnail() ) {
-              $thumbID = get_post_thumbnail_id( $post->ID );
-              $thumb = get_the_post_thumbnail_url( get_the_ID(), 'full' );
-              $thumalt = get_post_meta( $thumbID, '_wp_attachment_image_alt', true );
-            } else {
-              $thumb = "https://placehold.jp/24/f0f0f1/666/720x411.png?text=No%20Image";
-              $thumalt = "no image";
-            }
-            ?>
-
-          <div class="p-singleBlog__head">
-            <p class="p-singleBlog__category"><?php echo esc_html( $cat_name ); ?></p>
-            <p class="p-singleBlog__date"><?php echo esc_html( $date ); ?></p>
-          </div>
-          <h1 class="p-singleBlog__title"><?php the_title(); ?></h1>
-          <img class="p-singleBlog__img" src="<?php echo esc_url( $thumb ); ?>" alt="<?php echo ( $thumalt ); ?>">
-          <?php the_content() ?>
-        </div><!-- p-singleBlog__contents -->
-      </div><!-- l-inner--800 p-singleBlog__inner-->
-
-      <div class="p-singleBlog__nav">
-        <?php
-        $prev = get_previous_post();
-        $next = get_next_post();
+        // サムネイル
+        if ( has_post_thumbnail() ) {
+          $thumbID = get_post_thumbnail_id( $post->ID );
+          $thumb = get_the_post_thumbnail_url( get_the_ID(), 'full' );
+          $thumalt = get_post_meta( $thumbID, '_wp_attachment_image_alt', true );
+        } else {
+          $thumb = "https://placehold.jp/24/f0f0f1/666/720x411.png?text=No%20Image";
+          $thumalt = "no image";
+        }
         ?>
-        <div class="l-inner--lower">
-          <ul class="c-navLinks">
-            <?php if ( $prev ): ?>
-              <li class="c-navLinks__prev">
-                <a href="<?php echo esc_url( get_permalink( $prev ) ); ?>" rel="prev">
-                  <span class="c-navLinks__label">
-                    <span><?php echo esc_html( get_the_title( $prev ) ); ?></span>
-                    <span><?php echo esc_html( get_the_modified_time( 'Y.m.d', $prev ) ); ?></span>
-                  </span>
-                </a>
-              </li>
-            <?php else: ?>
-              <li></li>
-            <?php endif; ?>
+        <div class="l-inner--800 p-singleBlog__inner">
+          <div class="p-singleBlog__contents">
 
-            <?php if ( $next ): ?>
-              <li class="c-navLinks__next">
-                <a href="<?php echo esc_url( get_permalink( $next ) ); ?>">
-                  <span class="c-navLinks__label">
-                    <span><?php echo esc_html( get_the_title( $next ) ); ?></span>
-                    <span><?php echo esc_html( get_the_modified_time( 'Y.m.d', $next ) ); ?></span>
-                  </span>
-                </a>
-              </li>
-            <?php else: ?>
-              <li></li>
-            <?php endif; ?>
-          </ul>
-          
+            <div class="p-singleBlog__head">
+              <p class="p-singleBlog__category"><?php echo esc_html( $cat_name ); ?></p>
+              <p class="p-singleBlog__date"><?php echo esc_html( $date ); ?></p>
+            </div>
+            <h1 class="p-singleBlog__title"><?php the_title(); ?></h1>
+            <img class="p-singleBlog__img" src="<?php echo esc_url( $thumb ); ?>" alt="<?php echo ( $thumalt ); ?>">
+            <div class="p-singleBlog__body c-content">
+              <?php the_content() ?>
+            </div>
+          </div><!-- p-singleBlog__contents -->
+        </div><!-- l-inner--800 p-singleBlog__inner-->
+
+        <div class="p-singleBlog__nav">
           <?php
-            endwhile;
-              wp_reset_postdata();
-            endif;
+          $prev = get_previous_post();
+          $next = get_next_post();
           ?>
+          <div class="l-inner--lower">
+            <ul class="c-navLinks">
+              <?php if ( $prev ): ?>
+                <li class="c-navLinks__prev">
+                  <a href="<?php echo esc_url( get_permalink( $prev ) ); ?>" rel="prev">
+                    <span class="c-navLinks__label">
+                      <span><?php echo esc_html( get_the_title( $prev ) ); ?></span>
+                      <span><?php echo esc_html( get_the_modified_time( 'Y.m.d', $prev ) ); ?></span>
+                    </span>
+                  </a>
+                </li>
+              <?php else: ?>
+                <li></li>
+              <?php endif; ?>
 
-        </div>
-      </div>
+              <?php if ( $next ): ?>
+                <li class="c-navLinks__next">
+                  <a href="<?php echo esc_url( get_permalink( $next ) ); ?>">
+                    <span class="c-navLinks__label">
+                      <span><?php echo esc_html( get_the_title( $next ) ); ?></span>
+                      <span><?php echo esc_html( get_the_modified_time( 'Y.m.d', $next ) ); ?></span>
+                    </span>
+                  </a>
+                </li>
+              <?php else: ?>
+                <li></li>
+              <?php endif; ?>
+            </ul>
+          </div><!-- l-inner--lower -->
+        </div><!-- p-singleBlog__nav -->
+      <?php
+        endwhile;
+          wp_reset_postdata();
+        endif;
+      ?>
     </div><!-- l-main__body -->
   </main>
 
